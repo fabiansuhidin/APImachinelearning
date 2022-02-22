36486a990523a6c856c33b37e7cc968e24e50ab6
fabiansuhidin <69500254+fabiansuhidin@users.noreply.github.com>
2022-02-22 20:08:15 +0700

    Create function.py

---
+++ function.py
@@ -0,0 +1,10 @@
+def prepare_image(img):
+    img = Image.open(io.BytesIO(img))
+    img = img.resize((224, 224))
+    img = np.array(img)
+    img = np.expand_dims(img, 0)
+    return img
+
+
+def predict_result(img):
+    return 1 if model.predict(img)[0][0] > 0.5 else 0
