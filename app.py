35f98d53388f58c901a384531874682545725ea3
fabiansuhidin <69500254+fabiansuhidin@users.noreply.github.com>
2022-03-08 19:05:32 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,36 @@ import numpy as np
 import tensorflow as tf
 from PIL import Image
 from flask import Flask, jsonify, request
+import cv2
+from kafka import KafkaProducer
+
+#  connect to Kafka
+producer = KafkaProducer(bootstrap_servers='localhost:9092')
+
+def video_emitter(video):
+    # Open the video
+    video = cv2.VideoCapture(video)
+    print(' emitting.....')
+
+    # read the file
+    while (video.isOpened):
+        # read the image in each frame
+        success, image = video.read()
+        # check if the file has read to the end
+        if not success:
+            break
+        # convert the image png
+        ret, jpeg = cv2.imencode('.png', image)
+        # Convert the image to bytes and send to kafka
+        producer.send(topic, jpeg.tobytes())
+        # To reduce CPU usage create sleep time of 0.2sec  
+        time.sleep(0.2)
+    # clear the capture
+    video.release()
+    print('done emitting')
+
+if __name__ == '__main__':
+    video_emitter('video.mp4')
 
 model = tf.keras.models.load_model('tenserflowdatabase/food101')
 
