b9aa6d407e34fbd72bada3a8eaa32507a15f9bd2
fabiansuhidin <69500254+fabiansuhidin@users.noreply.github.com>
2022-03-15 18:58:42 +0700

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,38 @@ from PIL import Image
 from flask import Flask, jsonify, request
 import cv2
 from kafka import KafkaProducer
+from flask_restful import Resource, Api
+  
+
+# creating an API object
+api = Api(app)
+  
+# making a class for a particular resource
+class Hello(Resource):
+  
+    # corresponds to the GET request.
+    def get(self):
+  
+        return jsonify({'message': 'hello world'})
+  
+    # Corresponds to POST request
+    def post(self):
+          
+        data = request.get_json()     # status code
+        return jsonify({'data': data}), 201
+  
+  
+# another resource to calculate the square of a number
+class Square(Resource):
+  
+    def get(self, num):
+  
+        return jsonify({'square': num**2})
+  
+  
+# adding the defined resources along with their corresponding urls
+api.add_resource(Hello, '/')
+api.add_resource(Square, '/square/<int:num>')
 
 #  connect to Kafka
 producer = KafkaProducer(bootstrap_servers='localhost:9092')
